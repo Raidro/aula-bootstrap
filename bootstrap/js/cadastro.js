022654f09ab3838b4de1ccd36a76df7aa3281eb2
MATEUS PERREIRA SATIRO REYES <a9261456@rn.senac.br>
2019-07-22 20:02:11 -0300

    adicionado o formulario

--- bootstrap/js/cadastro.js
+++ bootstrap/js/cadastro.js
@@ -1,10 +1,17 @@
 
 function cadastrar() {
 
+    let formulario = [];
+
     let nome = document.getElementById('nome').value;
+    formulario.push(nome);
+
+    let fone = document.getElementById('fone').value;
+    formulario.push(fone);
 
     let cidade = document.getElementById('cidade').value;
+    formulario.push(cidade);
 
-    console.log(nome);
+    console.log(formulario);
 
 }
